155ebbe987b81b86e4bd8954ff7b34cbc402768e
Andres Figueroa <andres.figueroa,a@gmail.com>
2021-05-13 02:04:23 -0400

    Added bounding box code

--- App/packages/functions.py
+++ App/packages/functions.py
@@ -1,2 +1,23 @@
+from math import sin, cos, asin, sqrt, degrees, radians
 
+RADIUS = 6371.0
 
+def bounding_box(lat, lon, distance):
+    dlat = distance / RADIUS
+    dlon = asin(sin(dlat) / cos(radians(lat)))
+    return degrees(dlat), degrees(dlon)
+
+    
+def studentsInClasses(student_list, classroom_list):
+    for classroom in classroom_list:
+        #Assumes we are far from the poles and the +/- 180 longitude
+        dlat, dlong = bounding_box(classroom['latitude'],classroom['longitude'],10)
+               
+        maxlat = classroom['latitude'] + dlat
+        minlat = classroom['latitude'] - dlat
+        
+        maxlong = classroom['longitude'] + dlong
+        minlong = classroom['longitude'] - dlong
+        
+        
+    return 1
